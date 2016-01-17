bc9ff1097315cef0c0638faef1a1b124e6ffd5cd
Isaac Mast <isaac.k.mast@gmail.com>
2016-01-16 20:20:26 -0500

    Created Node class initial public interface.

---
+++ Node.java
@@ -0,0 +1,18 @@
+public class Node {
+
+	public void setLeftChild(int value) {
+
+	}
+
+	public int getLeftChild() {
+
+	}
+
+	public void setRightChild(int value) {
+
+	}
+
+	public int getRightChild() {
+
+	}
+}
