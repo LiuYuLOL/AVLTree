941485e58b568247678a9f603b06d7e55093a937
Isaac Mast <isaac.k.mast@gmail.com>
2016-01-16 20:50:32 -0500

    Documented the AVLTree class public interface

--- AVLTree.java
+++ AVLTree.java
@@ -1,10 +1,26 @@
+/**
+ * Implementation of AVL tree data structure that holds Node ojbects 
+ * that stores an integer as its data.
+ * @author Isaac Mast
+ */
 public class AVLTree {
 
-	public void insert(int value) {
+	/**
+	 * Creates a new Node object that contains an integer, data, and 
+	 * inserts it into the AVL tree. 
+	 * @param data integer to be stored in the new Node object
+	 */
+	public void insert(int data) {
 
 	}
 
-	public boolean find(int value) {
+	/**
+	 * Finds a Node object in the AVL tree that contains the integer, 
+	 * data.
+	 * @return True if a Node object is found in the AVL tree that 
+	 * contains the integer, data
+	 */
+	public boolean find(int data) {
 
 	}
 }
