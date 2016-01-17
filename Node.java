2f709bc7f10a7959d8b480942941c82afca58568
Isaac Mast <isaac.k.mast@gmail.com>
2016-01-16 21:35:49 -0500

    Documented public interface and changed method declarations and argument variables.

--- Node.java
+++ Node.java
@@ -1,18 +1,42 @@
+/**
+ * Node for an AVL tree or binary search tree that holds an 
+ * integer, data, and pointers to its left and right children, 
+ * leftChild and rightChild, respectively. 
+ * @author Isaac Mast
+ */
 public class Node {
 
-	public void setLeftChild(int value) {
+	/**
+	 * Sets the data value of the left child of the current node.
+	 * @param data the integer to set the data value to for the left 
+	 * child of the current node 
+	 */
+	public void setLeftChild(int data) {
 
 	}
 
-	public int getLeftChild() {
+	/**
+	 * Returns the left child node of the current node.
+	 * @return leftChild the left child of the current node
+	 */
+	public Node getLeftChild() {
 
 	}
 
-	public void setRightChild(int value) {
+	/**
+	 * Sets the data value of the right child of the current node.
+	 * @param data the integer to set the data value to for the right 
+	 * child of the current node
+	 */
+	public void setRightChild(int data) {
 
 	}
 
-	public int getRightChild() {
+	/**
+	 * Returns the right child node of the current node.
+	 * @return rightChild the right child of the current node
+	 */
+	public Node getRightChild() {
 
 	}
 }
