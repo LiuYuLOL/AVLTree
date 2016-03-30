41898574433b25e8009738b05d7ab6bea0c79d52
Isaac Mast <isaac.k.mast@gmail.com>
2016-03-30 18:10:58 -0400

    Changed find() method to check if current.getData() == data first

--- AVLTree.java
+++ AVLTree.java
@@ -43,15 +43,15 @@ public class AVLTree {
 	 */
 	public boolean find(int data) {
 		Node current = root;
+		if (current.getData() == data) {
+			return True;
+		}
 		while (current.getData() != data && current != null) {
 			if (data < current.data) {
 				current = root.getLeftChild();
 			} else {
 				current = root.getRightChild();
 			}
-		}
-		if (current.getData() == data) {
-			return True;
 		}
 		return False;
 	}
