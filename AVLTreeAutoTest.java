9914dc060f136a31093ee600ad51c6f4485096cb
Isaac Mast <isaac.k.mast@gmail.com>
2016-04-03 14:52:09 -0400

    Implemented test for find() method

--- AVLTreeAutoTest.java
+++ AVLTreeAutoTest.java
@@ -21,6 +21,19 @@ public class AVLTreeAutoTest extends AVLTreeTest {
 			print(e.toString(), true);
 		}
 
+		// test find()
+		try {
+			print(TESTING_FIND, true);
+			for (int i: values) {
+				print(FINDING + i + ELLIPSIS, false);
+				if (tree.find(i)) {
+					print(SUCCESS, true);
+				} else {
+					print(FAILED, true);
+				}
+			}
+		} catch (Exception e) {
+			print(e.toString(), true);
 		}
 	}
 }
